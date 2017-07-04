a85bf9a203938d746bd51881be47fc05a0721056
Kristina Del Rio Albrechet <kristinadelrioalbrechet@macminissu4.training.local>
2017-07-04 13:43:31 +0300

    create static class Validator

--- Calculator/InputAdapter.swift
+++ Calculator/InputAdapter.swift
@@ -11,186 +11,38 @@ import Foundation
 class IntputAdapter: InputProtocol {
     static let shared = IntputAdapter()
     let brain = Brain.shared
-    var buffer: String!
     
     func enterNum(_ number: Int) {
-        if buffer == nil || buffer == "0" {
-            buffer = String(number)
-            
-        } else if buffer.characters.last == "." || buffer.characters.last! >= "0" && buffer.characters.last! <= "9" {
-            buffer = buffer + "\(number)"
-            
-        } else if buffer.characters.count == 1 && (buffer.characters.last == "+" || buffer.characters.last == "-")  {
-            buffer = buffer + "\(number)"
-        } else {
-            buffer = buffer + " \(number)"
-            
-        }
-        
-        brain.EnterEquation(equation: buffer)
+        Validator.validateNum(number)
+        brain.EnterEquation(equation: Validator.Buffer)
     }
     
-    // 10001 - equalButton 10002 - clear button
     func enterServiceKey(_ serviceKey: Int) {
-        if serviceKey == 10001 {
-            buffer = brain.equal()
-        } else {
-            buffer = nil
-            brain.clear()
+        switch serviceKey {
+        case 10013: Validator.Buffer = brain.equal()
+        case 10014: Validator.Buffer = nil; brain.clear()
+        default: break;
         }
     }
     
     func enterUtility(_ symbol: String) {
         switch symbol {
-        case "+" : validatePls()
-        case "-" : validateMns()
-        case "×" : validateMul()
-        case "÷" : validateDiv()
-        case "^" : validatePow()
-        case "." : validateDot()
-        case "(" : validateLeftBracket()
-        case ")" : validateRightBracket()
-        case "√" : validateSqrt()
-        case "ln" : validateLog()
-        case "sin" : validateSin()
-        case "cos" : validateCos()
+        case "+" : Validator.validatePls()
+        case "-" : Validator.validateMns()
+        case "×" : Validator.validateMul()
+        case "÷" : Validator.validateDiv()
+        case "^" : Validator.validatePow()
+        case "." : Validator.validateDot()
+        case "(" : Validator.validateLeftBracket()
+        case ")" : Validator.validateRightBracket()
+        case "√" : Validator.validateSqrt()
+        case "ln" : Validator.validateLog()
+        case "sin" : Validator.validateSin()
+        case "cos" : Validator.validateCos()
         default: break
         }
         
-        brain.presentHistory(currentInput: buffer)
-    }
-    
-    // =======================
-    
-    func validatePls() {
-        if buffer == nil || buffer == "0" || buffer.characters.count == 1 && buffer.characters.last == "-" {
-            buffer = "+"
-        } else if buffer.characters.last == ")" {
-            buffer = buffer + " +"
-            
-        } else if buffer.characters.last! >= "0" && buffer.characters.last! <= "9" {
-            buffer = buffer + " +"
-        } else {
-            buffer.characters.removeLast()
-            buffer = buffer + "+"
-        }
-    }
-        
-    func validateMns() {
-        if buffer == nil || buffer == "0" || buffer.characters.count == 1 && buffer.characters.last == "+" {
-            buffer = "-"
-        } else if buffer.characters.last == ")" {
-            buffer = buffer + " )"
-            
-        } else if buffer.characters.last! >= "0" && buffer.characters.last! <= "9" {
-            buffer = buffer + " -"
-        } else {
-            buffer.characters.removeLast()
-            buffer = buffer + "-"
-        }
-    }
-        
-    func validateDot() {
-        if buffer == nil || buffer == "0" {
-            buffer = "0."
-        } else if buffer.characters.last != "." {
-            if buffer.characters.last! >= "0" && buffer.characters.last! <= "9" {
-                buffer! += "."
-            } else {
-                buffer = buffer + " 0."
-            }
-        }
-    }
-    
-        
-     func validateMul() {
-        if buffer != nil && buffer.characters.count > 1 {
-            if buffer.characters.last! >= "0" && buffer.characters.last! <= "9" {
-                buffer = buffer + " ×"
-            } else if buffer.characters.last == ")" {
-                buffer = buffer + " ×"
-                
-            } else {
-                buffer.characters.removeLast()
-                buffer = buffer + "×"
-            }
-        }
-    }
-        
-    func validateDiv() {
-        if buffer != nil && buffer.characters.count > 1 {
-            if buffer.characters.last! >= "0" && buffer.characters.last! <= "9" {
-                buffer = buffer + " ÷"
-            } else if buffer.characters.last == ")" {
-                buffer = buffer + " ÷"
-                
-            } else {
-                buffer.characters.removeLast()
-                buffer = buffer + "÷"
-            }
-        }
+        brain.presentHistory(currentInput: Validator.Buffer)
     }
 
-        
-    func validatePow() {
-        if (buffer != nil && buffer != "0") && buffer.characters.count >= 1 {
-            if buffer.characters.last! >= "0" && buffer.characters.last! <= "9" {
-                buffer = buffer + " ^"
-            } else if buffer.characters.last == ")" {
-                buffer = buffer + " ^"
-                
-            } else {
-                buffer.characters.removeLast()
-                buffer = buffer + "^"
-            }
-        }
-    }
-        
-    func validateSqrt() {
-            buffer == nil || buffer == "0" ? (buffer = "√") : (buffer = buffer + " √")
-        }
-    
-    func validateLog() {
-        buffer == nil || buffer == "0" ? (buffer = "ln (") : (buffer = buffer + " ln (")
-        brain.countLeftBrackets += 1
-    }
-    
-    func validateSin() {
-        buffer == nil || buffer == "0" ? (buffer = "sin (") : (buffer = buffer + " sin (")
-        brain.countLeftBrackets += 1
-    }
-    
-    func validateCos() {
-        buffer == nil || buffer == "0" ? (buffer = "cos (") : (buffer = buffer + " cos (")
-        brain.countLeftBrackets += 1
-    }
-    
-    func validateLeftBracket() {
-        if buffer == nil || buffer == "0" {
-            buffer = "("
-            brain.countLeftBrackets += 1
-        } else if buffer.characters.last! >= "0" && buffer.characters.last! <= "9" {
-            buffer = buffer + " × ("
-            brain.countLeftBrackets += 1
-        } else {
-            buffer = buffer + " ("
-            brain.countLeftBrackets += 1
-        }
-    }
-
-    func validateRightBracket() {
-        if brain.countLeftBrackets != 0 {
-            if buffer.characters.last! >= "0" && buffer.characters.last! <= "9" ||
-                (buffer.characters.last == ")" && brain.countLeftBrackets > brain.countRightBrackets) {
-                
-                buffer = buffer + " )"
-                brain.countRightBrackets += 1
-            } else {
-                buffer.characters.removeLast()
-                buffer = buffer + ")"
-                brain.countRightBrackets += 1
-            }
-        }
-    }
-    
 }
---
+++ Calculator/Validator.swift
@@ -0,0 +1,158 @@
+//
+//  Validator.swift
+//  Calculator
+//
+//  Created by Kristina Del Rio Albrechet on 7/4/17.
+//  Copyright © 2017 Kristina Del Rio Albrechet. All rights reserved.
+//
+
+import Foundation
+
+class Validator {
+    private static var buffer: String!
+    static var Buffer: String! { set { buffer = newValue } get { return self.buffer } }
+    
+    static func validateNum(_ num: Int) {
+        if buffer == nil || buffer == "0" {
+            buffer = String(num)
+            
+        } else if buffer.characters.last == "." || buffer.characters.last! >= "0" && buffer.characters.last! <= "9" {
+            buffer = buffer + "\(num)"
+            
+        } else if buffer.characters.count == 1 && (buffer.characters.last == "+" || buffer.characters.last == "-")  {
+            buffer = buffer + "\(num)"
+        } else {
+            buffer = buffer + " \(num)"
+            
+        }
+        
+    }
+    static func validatePls() {
+        if buffer == nil || buffer == "0" || buffer.characters.count == 1 && buffer.characters.last == "-" {
+            buffer = "+"
+        } else if buffer.characters.last == ")" {
+            buffer = buffer + " +"
+            
+        } else if buffer.characters.last! >= "0" && buffer.characters.last! <= "9" {
+            buffer = buffer + " +"
+        } else {
+            buffer.characters.removeLast()
+            buffer = buffer + "+"
+        }
+    }
+    
+    static func validateMns() {
+        if buffer == nil || buffer == "0" || buffer.characters.count == 1 && buffer.characters.last == "+" {
+            buffer = "-"
+        } else if buffer.characters.last == ")" {
+            buffer = buffer + " )"
+            
+        } else if buffer.characters.last! >= "0" && buffer.characters.last! <= "9" {
+            buffer = buffer + " -"
+        } else {
+            buffer.characters.removeLast()
+            buffer = buffer + "-"
+        }
+    }
+    
+    static func validateDot() {
+        if buffer == nil || buffer == "0" {
+            buffer = "0."
+        } else if buffer.characters.last != "." {
+            if buffer.characters.last! >= "0" && buffer.characters.last! <= "9" {
+                buffer! += "."
+            } else {
+                buffer = buffer + " 0."
+            }
+        }
+    }
+    
+    static func validateMul() {
+        if buffer != nil && buffer.characters.count > 1 {
+            if buffer.characters.last! >= "0" && buffer.characters.last! <= "9" {
+                buffer = buffer + " ×"
+            } else if buffer.characters.last == ")" {
+                buffer = buffer + " ×"
+                
+            } else {
+                buffer.characters.removeLast()
+                buffer = buffer + "×"
+            }
+        }
+    }
+    
+    static func validateDiv() {
+        if buffer != nil && buffer.characters.count > 1 {
+            if buffer.characters.last! >= "0" && buffer.characters.last! <= "9" {
+                buffer = buffer + " ÷"
+            } else if buffer.characters.last == ")" {
+                buffer = buffer + " ÷"
+                
+            } else {
+                buffer.characters.removeLast()
+                buffer = buffer + "÷"
+            }
+        }
+    }
+    
+    static func validatePow() {
+        if (buffer != nil && buffer != "0") && buffer.characters.count >= 1 {
+            if buffer.characters.last! >= "0" && buffer.characters.last! <= "9" {
+                buffer = buffer + " ^"
+            } else if buffer.characters.last == ")" {
+                buffer = buffer + " ^"
+                
+            } else {
+                buffer.characters.removeLast()
+                buffer = buffer + "^"
+            }
+        }
+    }
+    
+    static func validateSqrt() {
+        buffer == nil || buffer == "0" ? (buffer = "√") : (buffer = buffer + " √")
+    }
+    
+    static func validateLog() {
+        buffer == nil || buffer == "0" ? (buffer = "ln (") : (buffer = buffer + " ln (")
+        Brain.shared.countLeftBrackets += 1
+    }
+    
+    static func validateSin() {
+        buffer == nil || buffer == "0" ? (buffer = "sin (") : (buffer = buffer + " sin (")
+        Brain.shared.countLeftBrackets += 1
+    }
+    
+    static func validateCos() {
+        buffer == nil || buffer == "0" ? (buffer = "cos (") : (buffer = buffer + " cos (")
+        Brain.shared.countLeftBrackets += 1
+    }
+    
+    static func validateLeftBracket() {
+        if buffer == nil || buffer == "0" {
+            buffer = "("
+            Brain.shared.countLeftBrackets += 1
+        } else if buffer.characters.last! >= "0" && buffer.characters.last! <= "9" {
+            buffer = buffer + " × ("
+            Brain.shared.countLeftBrackets += 1
+        } else {
+            buffer = buffer + " ("
+            Brain.shared.countLeftBrackets += 1
+        }
+    }
+    
+    static func validateRightBracket() {
+        if Brain.shared.countLeftBrackets != 0 {
+            if buffer.characters.last! >= "0" && buffer.characters.last! <= "9" ||
+                (buffer.characters.last == ")" && Brain.shared.countLeftBrackets > Brain.shared.countRightBrackets) {
+                
+                buffer = buffer + " )"
+                Brain.shared.countRightBrackets += 1
+            } else {
+                buffer.characters.removeLast()
+                buffer = buffer + ")"
+                Brain.shared.countRightBrackets += 1
+            }
+        }
+    }
+}
